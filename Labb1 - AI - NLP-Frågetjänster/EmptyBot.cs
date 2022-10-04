87b49ceca8670eacb42f10d1cc88119646e0bc6d
johanlarsson1990 <johan.larsson@edu.edugrade.se>
2022-10-04 15:55:36 +0200

    first commit

---
+++ Labb1 - AI - NLP-Frågetjänster/EmptyBot.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with EmptyBot .NET Template version v4.17.1
+
+using Azure;
+using Azure.AI.TextAnalytics;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmptyBot
+{
+    public class EmptyBot : ActivityHandler
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<EmptyBot> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+     
+        string inputLanguage = "";
+        public EmptyBot(IConfiguration configuration, ILogger<EmptyBot> logger, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration;
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+          
+
+            var translator = new Translator()
+            {
+                translatorKey = _configuration["TranslatorKey"],
+                translatorEndpoint = _configuration["TranslatorEndpoint"],
+                credentail = new AzureKeyCredential(_configuration["KeyCredential"]),
+                analyticsEndpoint = new Uri(_configuration["AnalyticsEndpoint"])
+            };
+            
+            var detectClient = new TextAnalyticsClient(translator.analyticsEndpoint, translator.credentail);
+        await translator.LanguageDetection(detectClient, turnContext.Activity.Text, inputLanguage);
+          string translatedMessage =  await translator.TranslateToEnglish(turnContext.Activity.Text, inputLanguage);
+            turnContext.Activity.Text = translatedMessage;
+            var httpClient = _httpClientFactory.CreateClient();
+            var qnaMaker = new QnAMaker(new QnAMakerEndpoint
+            {
+                KnowledgeBaseId = _configuration["QnAKnowledgebaseId"],
+                EndpointKey = _configuration["QnAEndpointKey"],
+                Host = _configuration["QnAEndpointHostName"]
+            },
+            null,
+            httpClient);
+
+            _logger.LogInformation("Calling QnA Maker");
+
+            var options = new QnAMakerOptions { Top = 1 };
+           
+
+            // The actual call to the QnA Maker service.
+            var response = await qnaMaker.GetAnswersAsync(turnContext, options);
+            if (response != null && response.Length > 0)
+            {
+             
+                await turnContext.SendActivityAsync(MessageFactory.Text(response[0].Answer), cancellationToken);
+     
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("No QnA Maker answers were found."), cancellationToken);
+            }
+        }
+    }
+}
---
+++ Labb1 - AI - NLP-Frågetjänster/Translator.cs
@@ -0,0 +1,86 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyBot
+{
+    public class Translator
+{
+        //Translator keys
+        public string translatorKey;
+        public string translatorEndpoint;
+        public  readonly string location = "northeurope";
+
+        //Analytics keys
+        public AzureKeyCredential credentail;
+        public Uri analyticsEndpoint;
+
+
+     
+      public async Task<string> LanguageDetection(TextAnalyticsClient client, string inputString, string inputLanguage)
+        {
+            DetectedLanguage detectedLanguage =  client.DetectLanguage(inputString);
+             inputLanguage =  detectedLanguage.Iso6391Name;
+              return  inputLanguage;
+        }
+       public async Task <string>TranslateToEnglish(string textToTranslate, string inputLanguage)
+        {
+            string routeToBot = $"/translate?api-version=3.0&from={inputLanguage}&to=en";
+            object[] body = new object[] { new { Text = textToTranslate } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                // Build the request translator.
+
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(translatorEndpoint + routeToBot);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", translatorKey);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", location);
+
+                // Send the request and get response translator.
+
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                // Read response as a string.
+
+                string result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(result);
+                return result;
+            }
+        }
+        public async Task<string> TranslateToInput(string textToTranslate, string inputLanguage)
+        {
+            string routeToBot = $"/translate?api-version=3.0&from=en&to={inputLanguage}";
+            object[] body = new object[] { new { Text = textToTranslate } };
+            var requestBody = JsonConvert.SerializeObject(body);
+
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                // Build the request translator.
+
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(translatorEndpoint + routeToBot);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", translatorKey);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", location);
+
+                // Send the request and get response translator.
+
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                // Read response as a string.
+
+                string result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(result);
+                return result;
+            }
+        }
+    }
+}
